a9f37eaea239084c1bfc3c2ee220b87f009c8201
jaquanfl <jaquanf314@gmail.com>
2023-10-23 01:44:24 -0500

    1.1: Added inventory, removed "overdrafting", animated total sum text

--- app/src/main/java/com/example/cashcounter/MainActivity.java
+++ app/src/main/java/com/example/cashcounter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.cashcounter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,9 +12,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DecimalFormat;
+import android.animation.ObjectAnimator;
+import android.animation.AnimatorSet;
 
 public class MainActivity extends AppCompatActivity {
-    TextView totalText;
+    TextView totalText, inventory, inventory2;
     Button addOne, addFive, addTen, addTwenty, addFifty, addHundred;
     Button removeOne, removeFive, removeTen, removeTwenty, removeFifty, removeHundred;
     Button addPenny, addNickel, addDime, addQuarter;
@@ -19,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     Button clear;
 
     double totalSum = 0;
+    int onesCount, fivesCount, tensCount, twentyCount, fiftyCount, hundredCount;
+    int pennyCount, dimesCount, nickelsCount, quartersCount;
+
+
     DecimalFormat df = new DecimalFormat("#.##");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
          // Initialize views
         totalText = findViewById(R.id.totalText);
+        inventory = findViewById(R.id.inventory);
+        inventory2 = findViewById(R.id.inventory2);
+
         addOne = findViewById(R.id.addOne);
         addFive = findViewById(R.id.addFive);
         addTen = findViewById(R.id.addTen);
@@ -52,6 +64,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Initial text for total sum
         totalText.setText("$" + String.valueOf(df.format(totalSum)));
+        // Initial text for inventory
+        inventory.setText("$1: "+onesCount+"\n$5: "+fivesCount+"\n$10: "+tensCount+"\n$20: "+twentyCount+"\n$50: "+fiftyCount+"\n$100: "+hundredCount);
+        inventory2.setText("$0.01: "+pennyCount+"\n$0.05: "+nickelsCount+"\n$0.10: "+dimesCount+"\n$0.25: "+quartersCount);
 
 
 
@@ -62,60 +77,133 @@ public class MainActivity extends AppCompatActivity {
                 if (view.getId() == R.id.clear) {
                     Toast.makeText(getApplicationContext(),"To CLEAR: Press and Hold",Toast.LENGTH_SHORT).show();
                 } else if (view.getId() == R.id.addOne) {
+                    onesCount++;
                     totalSum++;
                 } else if (view.getId() == R.id.addFive) {
+                    fivesCount++;
                     totalSum += 5;
                 } else if (view.getId() == R.id.addTen) {
+                    tensCount++;
                     totalSum += 10;
                 } else if (view.getId() == R.id.addTwenty) {
+                    twentyCount++;
                     totalSum += 20;
                 } else if (view.getId() == R.id.addFifty) {
+                    fiftyCount++;
                     totalSum += 50;
                 } else if (view.getId() == R.id.addHundred) {
+                    hundredCount++;
                     totalSum += 100;
                 } else if (view.getId() == R.id.addPenny) {
+                    pennyCount++;
                     totalSum += 0.01;
                 } else if (view.getId() == R.id.addNickel) {
+                    nickelsCount++;
                     totalSum += 0.05;
                 } else if (view.getId() == R.id.addDime) {
+                    dimesCount++;
                     totalSum += 0.10;
                 } else if (view.getId() == R.id.addQuarter) {
+                    quartersCount++;
                     totalSum += 0.25;
-                } else if (view.getId() == R.id.removeOne) {
+                } else if (view.getId() == R.id.removeOne && onesCount > 0) {
+                    onesCount--;
                     totalSum--;
-                } else if (view.getId() == R.id.removeFive) {
+                } else if (view.getId() == R.id.removeFive && fivesCount > 0) {
+                    fivesCount--;
                     totalSum -= 5;
-                } else if (view.getId() == R.id.removeTen) {
+                } else if (view.getId() == R.id.removeTen && tensCount > 0) {
+                    tensCount--;
                     totalSum -= 10;
-                } else if (view.getId() == R.id.removeTwenty) {
+                } else if (view.getId() == R.id.removeTwenty && twentyCount > 0) {
+                    twentyCount--;
                     totalSum -= 20;
-                } else if (view.getId() == R.id.removeFifty) {
+                } else if (view.getId() == R.id.removeFifty && fiftyCount > 0) {
+                    fiftyCount--;
                     totalSum -= 50;
-                } else if (view.getId() == R.id.removeHundred) {
+                } else if (view.getId() == R.id.removeHundred && hundredCount > 0) {
+                    hundredCount--;
                     totalSum -= 100;
-                } else if (view.getId() == R.id.removePenny) {
+                } else if (view.getId() == R.id.removePenny && pennyCount > 0) {
+                    pennyCount--;
                     totalSum -= 0.01;
-                } else if (view.getId() == R.id.removeNickel) {
+                } else if (view.getId() == R.id.removeNickel && nickelsCount > 0) {
+                    nickelsCount--;
                     totalSum -= 0.05;
-                } else if (view.getId() == R.id.removeDime) {
+                } else if (view.getId() == R.id.removeDime && dimesCount > 0) {
+                    dimesCount--;
                     totalSum -= 0.10;
-                } else if (view.getId() == R.id.removeQuarter) {
+                } else if (view.getId() == R.id.removeQuarter && quartersCount > 0) {
+                    quartersCount--;
                     totalSum -= 0.25;
                 }
-                if(totalSum < 0) {
-                    totalSum = 0;
+
+                if(view.getId() == R.id.totalText){
+                    if(inventory.getVisibility() == View.INVISIBLE){
+                        totalText.setEnabled(false);
+                        ObjectAnimator translationAnimator = ObjectAnimator.ofFloat(totalText, "translationY", -totalText.getHeight());
+                        translationAnimator.setDuration(500);
+                        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(inventory, "alpha", 0f, 1f);
+                        alphaAnimator.setDuration(700);
+                        ObjectAnimator alphaAnimator2 = ObjectAnimator.ofFloat(inventory2, "alpha", 0f, 1f);
+                        alphaAnimator2.setDuration(700);
+
+                        Animator.AnimatorListener listener = new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                totalText.setEnabled(true);
+                            }
+                        };
+                        alphaAnimator.addListener(listener);
+
+                        AnimatorSet animatorSet = new AnimatorSet();
+                        animatorSet.playTogether(translationAnimator, alphaAnimator, alphaAnimator2);
+                        animatorSet.start();
+                        inventory.setVisibility(View.VISIBLE);
+                        inventory2.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        ObjectAnimator translationAnimator = ObjectAnimator.ofFloat(totalText, "translationY", 0);
+                        translationAnimator.setDuration(500);
+                        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(inventory, "alpha", 1f, 0f);
+                        alphaAnimator.setDuration(200);
+                        ObjectAnimator alphaAnimator2 = ObjectAnimator.ofFloat(inventory2, "alpha", 1f, 0f);
+                        alphaAnimator2.setDuration(200);
+
+                        Animator.AnimatorListener listener = new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                totalText.setEnabled(true);
+                                inventory.setVisibility(View.INVISIBLE);
+                                inventory2.setVisibility(View.INVISIBLE);
+                            }
+                        };
+                        alphaAnimator.addListener(listener);
+
+                        AnimatorSet animatorSet = new AnimatorSet();
+                        animatorSet.playTogether(translationAnimator, alphaAnimator, alphaAnimator2);
+                        animatorSet.start();
+                    }
                 }
 
                 // Update the totalText with the updated totalSum
                 totalText.setText("$" + String.valueOf(df.format(totalSum)));
+
+                // Update the inventory text
+                inventory.setText("$1: "+onesCount+"\n$5: "+fivesCount+"\n$10: "+tensCount+"\n$20: "+twentyCount+"\n$50: "+fiftyCount+"\n$100: "+hundredCount);
+                inventory2.setText("$0.01: "+pennyCount+"\n$0.05: "+nickelsCount+"\n$0.10: "+dimesCount+"\n$0.25: "+quartersCount);
+
             }
         };
 
         clear.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                totalSum = 0;
+                totalSum = onesCount = fivesCount = tensCount = twentyCount = fiftyCount = hundredCount = 0;
                 totalText.setText("$" + String.valueOf(df.format(totalSum)));
+                inventory.setText("$1: "+onesCount+"\n$5: "+fivesCount+"\n$10: "+tensCount+"\n$20: "+twentyCount+"\n$50: "+fiftyCount+"\n$100: "+hundredCount);
                 return true;
             }
         });
@@ -140,5 +228,6 @@ public class MainActivity extends AppCompatActivity {
         removeNickel.setOnClickListener(buttonClickListener);
         removeDime.setOnClickListener(buttonClickListener);
         removeQuarter.setOnClickListener(buttonClickListener);
+        totalText.setOnClickListener(buttonClickListener);
     }
 }
